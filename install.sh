5de7b92e68290515f8eb5e80462835b1c97691f7
Marcin Górski <32451056+gorskimarcin96@users.noreply.github.com>
2020-01-16 20:36:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,76 @@
 #PAMIETAJ O SPACJACH W PHPMYADMIN
 #spacja, tab, enter
 
-#apache2
+ssh -i .ssh/stefan root@164.132.42.211
+
+#firewall
+sudo ufw allow OpenSSH
+sudo ufw enable #-y
+sudo ufw allow 'Apache Full'
+
+#apache
 sudo apt update
 sudo apt install apache2 -y
+
+#VirtualHost
+echo '<VirtualHost *:80>' > /etc/apache2/sites-available/mgorski.dev.conf
+echo '  ServerName mgorski.dev' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '  ServerAlias www.mgorski.dev' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '  DocumentRoot "/var/www/html/public"' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '<Directory /var/www/>' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '  Options Indexes FollowSymLinks' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '  AllowOverride All' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '  Require all granted' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '  FallbackResource /index.php' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '</Directory>' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo 'ErrorLog ${APACHE_LOG_DIR}/error.log' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo 'CustomLog ${APACHE_LOG_DIR}/access.log combined' >> /etc/apache2/sites-available/mgorski.dev.conf
+echo '</VirtualHost>' >> /etc/apache2/sites-available/mgorski.dev.conf
+#nano /etc/apache2/sites-available/mgorski.dev.conf 
+sudo a2ensite mgorski.dev.conf 
+
+#VirtualHost 2
+echo '<VirtualHost *:80>' > /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '  ServerName forum.mgorski.dev' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '  ServerAlias www.forum.mgorski.dev' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '  DocumentRoot "/var/www/forum/public"' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '<Directory /var/www/>' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '  Options Indexes FollowSymLinks' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '  AllowOverride All' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '  Require all granted' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '  FallbackResource /index.php' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '</Directory>' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo 'ErrorLog ${APACHE_LOG_DIR}/error.log' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo 'CustomLog ${APACHE_LOG_DIR}/access.log combined' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+echo '</VirtualHost>' >> /etc/apache2/sites-available/forum.mgorski.dev.conf
+#nano /etc/apache2/sites-available/forum.mgorski.dev.conf 
+sudo a2ensite forum.mgorski.dev.conf 
+sudo a2dissite 000-default.conf
+sudo a2enmod rewrite
+clear
+sudo apache2ctl configtest
+sudo systemctl reload apache2
+sudo systemctl restart apache2
+
+#certbot
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update
+sudo apt-get install certbot python-certbot-apache
+sudo certbot --apache -d mgorski.dev -d www.mgorski.dev -m gorskimarcin96@gmail.com
+
+#php
+sudo apt install php libapache2-mod-php php-mysql -y
+echo '<IfModule mod_dir.c>' > /etc/apache2/mods-enabled/dir.conf
+echo '    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm' >> /etc/apache2/mods-enabled/dir.conf
+echo '</IfModule>' >> /etc/apache2/mods-enabled/dir.conf
+sudo apt install php-cli
 
 #mysql
 sudo apt install mysql-server -y
@@ -16,19 +83,19 @@ sudo mysql <<EOF
 	exit
 EOF
 
-#php
-sudo apt install php libapache2-mod-php php-mysql -y
-cd /etc/apache2/mods-enabled/
-echo '<IfModule mod_dir.c>' > dir.conf
-echo '    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm' >> dir.conf
-echo '</IfModule>' >> dir.conf
-sudo apt install php-cli
-
 #phpMyAdmin
 sudo apt update
 sudo apt install phpmyadmin php-mbstring php-gettext -y
 sudo phpenmod mbstring
 sudo systemctl restart apache2
+
+#mysql naprawa groupBy
+echo 'sql-mode=""' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo systemctl restart mysql
+
+#mysql dostęp do bazy danych z innych adresów (komentujemy linijkę bind-address)
+#/etc/mysql/mysql.conf.d/mysqld.cnf
+#bind-address = 127.0.0.1
 
 #composer
 sudo apt install composer -y
@@ -43,50 +110,4 @@ yarn install
 sudo apt-get install npm -y
 npm install -g @angular/cli
 sudo npm cache clean -f
-sudo npm install -g n
-
-#htaccess
-sudo a2enmod rewrite
-sudo systemctl restart apache2
-
-#mysql naprawa groupBy
-cd /etc/mysql/mysql.conf.d
-echo 'sql-mode=""' >> mysqld.cnf
-sudo systemctl restart mysql
-
-#mysql dostęp do bazy danych z innych adresów (komentujemy linijkę bind-address)
-/etc/mysql/mysql.conf.d/mysqld.cnf
-#bind-address = 127.0.0.1
-
-#VirtualHost
-#<VirtualHost *:80>
-#    DocumentRoot "/var/www/html/public"
-#    ServerName mgorski.dev
-#    ServerAlias www.mgorski.dev
-#
-#<Directory /var/www/>
-#    Options Indexes FollowSymLinks
-#    AllowOverride All
-#    Require all granted
-#    FallbackResource /index.php
-#</Directory>
-#
-#ErrorLog ${APACHE_LOG_DIR}/error.log
-#CustomLog ${APACHE_LOG_DIR}/access.log combined
-#</VirtualHost>
-#
-#<VirtualHost *:80>
-#    DocumentRoot "/var/www/html_test/public"
-#    ServerName test.mgorski.dev
-#    ServerAlias www.test.mgorski.dev
-#
-#<Directory /var/www/>
-#    Options Indexes FollowSymLinks
-#    AllowOverride All
-#    Require all granted
-#    FallbackResource /index.php
-#</Directory>
-#
-#ErrorLog ${APACHE_LOG_DIR}/test_error.log
-#CustomLog ${APACHE_LOG_DIR}/test_access.log combined
-#</VirtualHost>
+sudo npm install -g
